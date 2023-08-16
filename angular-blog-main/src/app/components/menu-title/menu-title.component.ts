4ef51003438011ed5a539259114ba66dcdb32ce1
LuisTakeo <luistakeo@hotmail.com>
2023-08-16 20:43:47 -0300

    Primeiro commit

---
+++ angular-blog-main/src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.css', './menu-title.responsive.component.css']
+})
+export class MenuTitleComponent {
+  @Input()
+  imgPrincipal: string = "";
+}
